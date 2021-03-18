938bedb8e9fae92041287621383d60be7c521030
Svetlana Khanina <khaninasn@gmail.com>
2021-03-18 13:57:42 +0300

    Add test php file

---
+++ index.php
@@ -0,0 +1,15 @@
+<h1> Hello from a Web Page </h1>
+<p>
+<?php
+echo "Hi there.\n";
+$x = 6*7;
+echo "The answer is ".$x.".And what is the question?\n";
+$vv = "Hello world";
+if(strpos($vv,"He") !== FALSE) echo "Right C\n";
+echo var_dump(FALSE);
+echo "\n\n";
+echo 'Test string: \n what happend?\n  \'\r \\ \t \"';
+echo "\nEnd\n";
+?>
+</p>
+<p>Another paragraph</p>
